ca14499478b9c859109ed93e4706d91103c6b39f
jingwei218 <yuanjingwei@aliyun.com>
2016-11-27 17:38:17 +0800

    updated on 26th Nov 2016

--- horizon/models.py
+++ horizon/models.py
@@ -5,17 +5,24 @@ class Project(models.Model):
     pass
 
 
-#计算表中的每一行
-class Element(models.Model):
+class Vendor(models.Model):
     pid = models.IntegerField(primary_key=True)
-    project = models.ManyToManyField(Project)
+    name = models.CharField(max_length=255, blank=False, null=False)
 
 
 class Service(models.Model):
     pid = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=30, blank=False)
     related_service = models.ManyToManyField('self')
-    element = models.ManyToManyField(Element)
+    level = models.IntegerField(blank=False, null=False)
+
+
+#计算表中的每一行
+class Element(models.Model):
+    pid = models.IntegerField(primary_key=True)
+    project = models.ManyToManyField(Project)
+    vendor = models.ForeignKey(Vendor)
+    service = models.ForeignKey(Service)
 
 
 #地理信息
@@ -23,34 +30,27 @@ class Geo(models.Model):
     pid = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=40, blank=False)
     related_geo = models.ForeignKey('self', null=True) #可回代自身，如：一个省有多个市
+    level = models.IntegerField(blank=False, null=False)
 
 
-#位置信息
-class Location(models.Model):
-    name = models.CharField(max_length=255, blank=False)
-    geo = models.ManyToManyField(Geo)
-    element = models.ManyToManyField(Element)
+class Quantity(models.Model):
+    quantity = models.FloatField(blank=False)
+    uom = models.CharField(max_length=10, blank=False)
+    element = models.ForeignKey(Element)
 
 
 #计费单位
-class ChargeableUnit(models.Model):
+class ChargeableQuantity(models.Model):
     pid = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=30, blank=False)
-    display_uom = models.CharField(max_length=10, blank=False) #显示的计费单位
-    base_uom = models.CharField(max_length=10, blank=True, null=True) #基准计量单位，如公斤
-    convertable_uom = models.CharField(max_length=10, blank=True, null=True) #需转换的计量单位，如：立方米
     converting_factor = models.FloatField(blank=True, null=True) #转换率，如：333公斤/立方米
-    comparible = models.CharField(max_length=3, choices=(('min', 'Minimum'),('max', 'Maximum')), blank=True, null=True) #比大比小
-    element = models.ManyToManyField(Element)
+    comparison = models.CharField(max_length=3, choices=(('min', 'Minimum'),('max', 'Maximum')), blank=True, null=True) #计算计费单位量的比较方式
+    target = models.IntegerField() #需要转换成计费单位的目标，指向该目标的序列的下标
+    base = models.IntegerField() #与转换后的计费单位进行比较的基准单位，指向该基准单位的下标
+    element = models.OneToOneField(Element)
 
     def __str__(self):
-        return self.name + ' ' + self.uom + ' ' + str(self.converting_factor) + ' ' + str(self.convertable_uom)
-
-
-class Price(models.Model):
-    pid = models.IntegerField(primary_key=True)
-    rate = models.FloatField(blank=True, null=True)
-    element = models.ForeignKey(Element)
+        return self.name + ' ' + str(self.converting_factor)
 
 
 class PriceCondition(models.Model):
@@ -58,7 +58,13 @@ class PriceCondition(models.Model):
     name = models.CharField(max_length=255, blank=False)
     low = models.FloatField(default=0)
     high = models.FloatField(default=0)
-    price = models.ManyToManyField(Price)
+
+
+class Price(models.Model):
+    pid = models.IntegerField(primary_key=True)
+    price = models.FloatField(blank=True, null=True)
+    price_condition = models.ForeignKey(PriceCondition)
+    element = models.ForeignKey(Element)
 
 
 #订单
